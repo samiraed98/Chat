f03e7bd9c2198569c914469d45caaacbdc5d3267
samiraed98 <139963477+samiraed98@users.noreply.github.com>
2024-10-16 01:50:21 +0300

    Update script.js

--- script.js
+++ script.js
@@ -77,7 +77,7 @@ const generateAPIResponse = async (incomingMessageDiv) => {
 // Show a loading animation while waiting for the API response
 const showLoadingAnimation = () => {
   const html = `<div class="message-content">
-                  <img class="avatar" src="images/gemini.svg" alt="Gemini avatar">
+                  <img class="avatar" src="./IMG_2783-removebg-preview.png" alt="Gemini avatar">
                   <p class="text"></p>
                   <div class="loading-indicator">
                     <div class="loading-bar"></div>
